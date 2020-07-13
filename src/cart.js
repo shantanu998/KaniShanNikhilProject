12beb14ad527aa29a2373b6af6beb511ab8d2fff
Kanishk Thakur <kanishkthakur98@gmail.com>
2020-07-13 18:17:24 +0530

    Server side rendering

--- src/cart.js
+++ src/cart.js
@@ -1,58 +1,64 @@
-import React, { Component, useState } from 'react';
+import React, { Component, useState } from "react";
 import store from "./store";
-import './cart.css'
-import { Link } from 'react-router-dom';
+import "./cart.css";
+import { Link } from "react-router-dom";
 import { Card, Image, Button, Container, ContainerMain } from "./styles";
 
-function cart({clist}){
-    
-//   const clist = props.children;
-  
-  if(clist===undefined||clist.prod == undefined || clist.prod.length==0){
-    return(
-        <div className="cart-empty">
-            <h2>You have not added any products to the cart yet !</h2>
-            <p>Please go through our E-shop and add products to this cart.</p>
-            <Link to={"/firstPage/"} style={{textDecoration:'none'}}><Button type="button"
-            className="btn btn-danger">Back</Button></Link>
-        </div>
+function cart({ clist }) {
+  //   const clist = props.children;
+
+  if (
+    clist === undefined ||
+    clist.prod == undefined ||
+    clist.prod.length == 0
+  ) {
+    return (
+      <div className="cart-empty">
+        <h2>You have not added any products to the cart yet !</h2>
+        <p>Please go through our E-shop and add products to this cart.</p>
+        <Link to={"/firstPage/"} style={{ textDecoration: "none" }}>
+          <Button type="button" className="btn btn-danger">
+            Back
+          </Button>
+        </Link>
+      </div>
     );
-  }  
+  }
 
-
-return(
+  return (
     <div className="cart">
-        <h3>E-Cart</h3>
-        <p>You have {store.getState().cart.prod.length} items waiting in your cart </p>
-        <div className="Products">
-            
-                {clist.prod.map((item) => {return(
-                
-                
-                    <div className="Item" key={item.cid}>
-                    
-                        <img src =  {item.url}  alt = {item.name} className="Img" />
-                    
-                        <h5 className="card-title">{item.name}</h5>
-                        <h6 className="card-subtitle mb-2 text-muted">
-                        { item.price}
-                        </h6>
-                        <Button type="button"
-            className="btn btn-danger" onClick={() =>
-                            store.dispatch({
-                                
-                                type: "DEL",
-                                payload: {
-                                id: item.cid,
-                                
-                                },
-                            })}
-                            > delete</Button>
-                    </div>
-                )})}
-        </div>
+      <h3>E-Cart</h3>
+      <p>
+        You have {store.getState().cart.prod.length} items waiting in your cart{" "}
+      </p>
+      <div className="Products">
+        {clist.prod.map((item) => {
+          return (
+            <div className="Item" key={item.cid}>
+              <img src={item.url} alt={item.name} className="Img" />
+
+              <h5 className="card-title">{item.name}</h5>
+              <h6 className="card-subtitle mb-2 text-muted">{item.price}</h6>
+              <Button
+                type="button"
+                className="btn btn-danger"
+                onClick={() =>
+                  store.dispatch({
+                    type: "DEL",
+                    payload: {
+                      id: item.cid,
+                    },
+                  })
+                }
+              >
+                {" "}
+                delete
+              </Button>
+            </div>
+          );
+        })}
+      </div>
     </div>
-
-
-)}
+  );
+}
 export default cart;
--- src/grid.js
+++ src/grid.js
@@ -8,33 +8,31 @@ import "bootstrap/dist/css/bootstrap.css";
 // import { Switch } from "antd";
 import store from "./store";
 import { Link } from "react-router-dom";
-import Loadable from 'react-loadable';
-import './navbar.css';
+import Loadable from "react-loadable";
+import "./navbar.css";
 
 const LoadableShop = Loadable({
-	loader: () => import('./firstPage'),
-	loading() {
-		return <div>Loading...</div>
-	}
-})
+  loader: () => import("./firstPage"),
+  loading() {
+    return <div>Loading...</div>;
+  },
+});
 
 const LoadableCart = Loadable({
-	loader: () => import('./cart'),
-	loading() {
-		return <div>Loading...</div>
-	}
-})
+  loader: () => import("./cart"),
+  loading() {
+    return <div>Loading...</div>;
+  },
+});
 
 const LoadableDetails = Loadable({
-	loader: () => import('./detailsPage'),
-	loading() {
-		return <div>Loading...</div>
-	}
-})
+  loader: () => import("./detailsPage"),
+  loading() {
+    return <div>Loading...</div>;
+  },
+});
 
-
-export default function Grid(){
-
+export default function Grid() {
   const [list, setList] = useState([]);
   useEffect(() => {
     store.dispatch({
@@ -59,29 +57,73 @@ export default function Grid(){
   console.log(88);
   //console.log(clist.length);
 
-
-    return (
-        <Router>
-          <nav className="nav-wrapper">
-            <div className="container">
-                <ul className="right">
-                    <li><Link to="/" className="brand-logo"><img src="https://www.bmcsoftware.com.au/content/experience-fragments/bmc/language-masters/en/customerspotlights/deutsche-telekomag/deutsche-telekomag/_jcr_content/root/customer_spotlight/logo.img.png" className='logo' alt='logo'/></Link></li>
-                    <li><Link to="/firstPage/" className="nav-text" style={{textDecoration:'none'}}>Shop</Link></li>
-                    <li><Link to="/cart/" className="nav-text" style={{textDecoration:'none'}}>My cart</Link></li>
-                    <li><Link to="/cart/"><img src="https://t7.rbxcdn.com/7944ec53125aaf6a278deaaa64976817" className='cart-logo' alt='cart-logo'></img></Link></li>
-                </ul>
-            </div>
-          </nav>
-          <Route
+  return (
+    <Router>
+      <nav className="nav-wrapper">
+        <div className="container">
+          <ul className="right">
+            <li>
+              <Link to="/" className="brand-logo">
+                <img
+                  src="https://www.bmcsoftware.com.au/content/experience-fragments/bmc/language-masters/en/customerspotlights/deutsche-telekomag/deutsche-telekomag/_jcr_content/root/customer_spotlight/logo.img.png"
+                  className="logo"
+                  alt="logo"
+                />
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/firstPage/"
+                className="nav-text"
+                style={{ textDecoration: "none" }}
+              >
+                Shop
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/cart/"
+                className="nav-text"
+                style={{ textDecoration: "none" }}
+              >
+                My cart
+              </Link>
+            </li>
+            <li>
+              <Link to="/cart/">
+                <img
+                  src="https://t7.rbxcdn.com/7944ec53125aaf6a278deaaa64976817"
+                  className="cart-logo"
+                  alt="cart-logo"
+                ></img>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <Route
         path="/:id"
-        exact strict
-        component={({ match }) => <LoadableDetails match={match} list={list}/>}></Route>
-          <Route path="/firstPage/" exact strict component={() => <LoadableShop list = {list}/>}>
-          </Route>
-          <Route path="/cart/" exact strict component={() => <LoadableCart clist = {clist}/>}>
-          </Route>
-          <Route path="/" exact component={() => <h1>Welcome to our E-Shop</h1>}></Route>
-        </Router>
-      );
-
+        exact
+        strict
+        component={({ match }) => <LoadableDetails match={match} list={list} />}
+      ></Route>
+      <Route
+        path="/firstPage/"
+        exact
+        strict
+        component={() => <LoadableShop list={list} />}
+      ></Route>
+      <Route
+        path="/cart/"
+        exact
+        strict
+        component={() => <LoadableCart clist={clist} />}
+      ></Route>
+      <Route
+        path="/"
+        exact
+        component={() => <h1>Welcome to our E-Shop</h1>}
+      ></Route>
+    </Router>
+  );
 }
